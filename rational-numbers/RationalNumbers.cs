66de1cdd12559d6afa7f3b5479b9aa99261245b2
rodion999 <info@devrabbit.net>
2018-06-01 17:13:31 +0600

    Rational numbers ready task

--- rational-numbers/RationalNumbers.cs
+++ rational-numbers/RationalNumbers.cs
@@ -5,73 +5,96 @@ public static class RealNumberExtension
 {
     public static double Expreal(this int realNumber, RationalNumber r)
     {
-        throw new NotImplementedException("You need to implement this extension method.");
+        return Math.Pow(realNumber, (double)(r.numerator) / (double)r.denominator);
     }
 }
 
 public struct RationalNumber
 {
+    public int numerator;
+    public int denominator;
     public RationalNumber(int numerator, int denominator)
     {
+        this.numerator = numerator / GCD(numerator, denominator);
+        this.denominator = denominator / GCD(numerator, denominator);
+        if (this.denominator < 0)
+        {
+            this.denominator = -this.denominator;
+            this.numerator = -this.numerator;
+        }
     }
+
+    static int GCD(int a, int b) => b == 0 ? Math.Abs(a) : GCD(Math.Abs(b), Math.Abs(a) % Math.Abs(b));
+
 
     public RationalNumber Add(RationalNumber r)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int numerator = this.numerator * r.denominator + r.numerator * this.denominator;
+        int denominator = this.denominator * r.denominator;
+        return new RationalNumber(numerator, denominator);
     }
 
     public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        return r1.Add(r2);
     }
 
     public RationalNumber Sub(RationalNumber r)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int numerator = this.numerator * r.denominator - r.numerator * this.denominator;
+        int denominator = this.denominator * r.denominator;
+        return new RationalNumber(numerator, denominator);
     }
 
     public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        return r1.Sub(r2);
     }
 
     public RationalNumber Mul(RationalNumber r)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int numerator = this.numerator * r.numerator;
+        int denominator = this.denominator * r.denominator;
+        return new RationalNumber(numerator, denominator);
     }
 
     public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        return r1.Mul(r2);
     }
 
     public RationalNumber Div(RationalNumber r)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int numerator = this.numerator * r.denominator;
+        int denominator = this.denominator * r.numerator;
+
+        return new RationalNumber(numerator, denominator);
     }
 
     public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
     {
-        throw new NotImplementedException("You need to implement this operator.");
+        return r1.Div(r2);
     }
 
     public RationalNumber Abs()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.numerator < 0 ? new RationalNumber(-this.numerator, this.denominator) : new RationalNumber(this.numerator, this.denominator);
     }
 
     public RationalNumber Reduce()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new RationalNumber(this.numerator, this.denominator);
     }
 
     public RationalNumber Exprational(int power)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.numerator = (int)Math.Pow((double)this.numerator, (double)power);
+        this.denominator = (int)Math.Pow((double)this.denominator, (double)power);
+        return new RationalNumber(this.numerator, this.denominator);
     }
 
     public double Expreal(int baseNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Pow(baseNumber, (double)numerator / (double)denominator);
     }
 }
