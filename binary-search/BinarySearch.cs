b511ba4a24ef4b590007d34275cade687316faad
rodion999 <info@devrabbit.net>
2018-06-08 14:00:02 +0600

    Binary-search is solved

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class BinarySearch
+{
+    int[] _array;
+    public BinarySearch(int[] input)
+    {
+        _array = input;
+    }
+
+    public int Find(int value)
+    {
+        int pos = Array.BinarySearch(_array, value);
+        if (pos >= 0)
+            return pos;
+        else return -1;
+    }
+}
+
